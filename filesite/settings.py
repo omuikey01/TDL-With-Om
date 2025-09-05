6dc1631d3a3b4f5b16827f152a8519acd83d969d
Om Uikey <uikeyom1234@gmail.com>
2025-09-05 13:53:10 +0530

    PROVIDE PERMISSION ON ALLOWED HOST AND DEBUG = FALSE

--- filesite/settings.py
+++ filesite/settings.py
@@ -17,10 +17,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -------------------------
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-secret')
 
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+# DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    ".onrender.com",   # allow any subdomain of Render
+    "filesite.onrender.com",  # replace with your exact Render URL
+    "localhost",
+    "127.0.0.1"
+]
 
 # -------------------------
 # Application definition
